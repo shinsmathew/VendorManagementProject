10e5e53c550589c03e7a795b5f8251e66cb27892
shinsmathew <shinsmathew2537@gmail.com>
2025-03-21 14:05:42 +0100

    changes

--- VendorManagementProject.Tests/RepositotyTest/AuthRepositoryTests.cs
+++ VendorManagementProject.Tests/RepositotyTest/AuthRepositoryTests.cs
@@ -34,20 +34,24 @@ namespace VendorManagementProject.Tests.RepositotyTest
             // Arrange
             var user = new VendorUser
             {
-                UserID = "john.doe",
-                Password = "Password@123"
+                UserFirstName = "Shins",
+                UserLastName = "Mathew",
+                UserID = "Shins.Mathew",
+                Password = "Shins@123",
+                Email = "shinsm@gmail.com",
+                Role = "Admin"
             };
 
             var mockDbSet = new Mock<DbSet<VendorUser>>();
             _mockContext.Setup(ctx => ctx.VendorUsers).Returns(mockDbSet.Object);
             _mockPasswordHasher.Setup(hasher => hasher.HashPassword(user.Password)).Returns("hashedPassword");
-            _mockTokenService.Setup(service => service.GenerateToken(user)).Returns("fakeToken");
+            _mockTokenService.Setup(service => service.GenerateToken(user)).Returns("Generated_Fake_Token");
 
             // Act
             var result = await _authRepository.Register(user);
 
             // Assert
-            result.Should().Be("fakeToken");
+            result.Should().Be("Generated_Fake_Token");
             mockDbSet.Verify(dbSet => dbSet.AddAsync(user, default), Times.Once);
             _mockContext.Verify(ctx => ctx.SaveChangesAsync(default), Times.Once);
         }
@@ -56,19 +60,19 @@ namespace VendorManagementProject.Tests.RepositotyTest
         public async Task Login_WithValidCredentials_ReturnsToken()
         {
             // Arrange
-            var user = new VendorUser { UserID = "john.doe", Password = "hashedPassword" };
+            var user = new VendorUser { UserID = "Shins.Mathew", Password = "hashedPassword" };
             var mockDbSet = new Mock<DbSet<VendorUser>>();
             mockDbSet.Setup(dbSet => dbSet.FirstOrDefaultAsync(It.IsAny<System.Linq.Expressions.Expression<System.Func<VendorUser, bool>>>(), default))
                      .ReturnsAsync(user);
             _mockContext.Setup(ctx => ctx.VendorUsers).Returns(mockDbSet.Object);
-            _mockPasswordHasher.Setup(hasher => hasher.VerifyPassword("Password@123", user.Password)).Returns(true);
-            _mockTokenService.Setup(service => service.GenerateToken(user)).Returns("fakeToken");
+            _mockPasswordHasher.Setup(hasher => hasher.VerifyPassword("Shins@123", user.Password)).Returns(true);
+            _mockTokenService.Setup(service => service.GenerateToken(user)).Returns("Generated_Fake_Token");
 
             // Act
-            var result = await _authRepository.Login("john.doe", "Password@123");
+            var result = await _authRepository.Login("Shins.Mathew", "Shins@123");
 
             // Assert
-            result.Should().Be("fakeToken");
+            result.Should().Be("Generated_Fake_Token");
         }
     }
 }
